fcca2c41df95a1b6b52ec80080da3dd4f3f2cd66
Bankole Ebenezer <bankoleebenezer111@gmail.com>
2026-03-29 06:28:17 -0700

    chore: update Firebase hosting cache file in backup directory

--- scripts/env-check.cjs
+++
@@ -1,4 +0,0 @@
-console.log('FIREBASE_CONFIG:', process.env.FIREBASE_CONFIG);
-console.log('GCLOUD_PROJECT:', process.env.GCLOUD_PROJECT);
-console.log('GOOGLE_CLOUD_PROJECT:', process.env.GOOGLE_CLOUD_PROJECT);
-console.log('VITE_FIREBASE_PROJECT_ID:', process.env.VITE_FIREBASE_PROJECT_ID);
--- scripts/read-unicode.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const filename = process.argv[2] || 'projects.txt';
-const content = fs.readFileSync(filename, 'utf16le');
-console.log(content);
-
-
